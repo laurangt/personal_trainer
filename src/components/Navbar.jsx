8bcb33f027bf2a2695bd6cd1b4f7826401cc9d91
Laura Nguyen Trong <laurangt@yahoo.com>
2023-02-05 20:25:09 +0100

    nutrition and navbar margin

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,14 @@ import { NavLink } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{backgroundColor: window.location.pathname === '/' ? 'black' : '#162E3B'}} className='flex items-center p-5 justify-between'>
+    <nav style={{backgroundColor: window.location.pathname === '/' ? 'black' : '#162E3B'}} className='flex items-center pt-5 pb-4 pr-40 pl-40 justify-between'>
       <img src='logo192.png' alt='' className='h-8 w-8 flex-none'/>
-      <div className='text-white text-xl pr-16'>
-        <NavLink to="/" className='p-10'>Home</NavLink>
-        <NavLink to="/" className='p-10'>Coaching</NavLink>
-        <NavLink to="/" className='p-10'>Programmes</NavLink>
-        <NavLink to="/nutrition" className='p-10'>Nutrition</NavLink>
-        <NavLink to="/aboutme" className='p-10'>About me</NavLink>
+      <div className='text-white text-xl'>
+        <NavLink to="/" className='pl-10'>Home</NavLink>
+        <NavLink to="/" className='pl-10'>Coaching</NavLink>
+        <NavLink to="/" className='pl-10'>Programmes</NavLink>
+        <NavLink to="/nutrition" className='pl-10'>Nutrition</NavLink>
+        <NavLink to="/aboutme" className='pl-10'>About me</NavLink>
       </div>
     </nav>
   )
