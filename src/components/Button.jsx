e985ec2eb890e021c36236a850a70f7bd0e473d0
Laura Nguyen Trong <laurangt@yahoo.com>
2023-02-05 17:49:40 +0100

    navbar color change

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Button(props) {
   return (
-    <button className="border bg-yellow text-black w-64 h-14 rounded-lg">
+    <button className="mr-14 text-2xl font-bold border bg-yellow text-black w-64 h-14 rounded-lg">
       <p>{props.text}</p>
     </button>
   )
