b20093f1da7a49eaee1437a434f61ab6c0134e4d
Laura Nguyen Trong <laurangt@yahoo.com>
2023-02-06 18:47:45 +0100

    link of navbar to home#id part

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
+import { HashLink as Link } from 'react-router-hash-link';
 
 function Navbar() {
   return (
@@ -7,8 +8,8 @@ function Navbar() {
       <img src='logo192.png' alt='' className='h-8 w-8 flex-none'/>
       <div className='text-white text-xl'>
         <NavLink to="/" className='pl-10'>Home</NavLink>
-        <NavLink to="/" className='pl-10'>Coaching</NavLink>
-        <NavLink to="/" className='pl-10'>Programmes</NavLink>
+        <Link to="/#1on1coaching" className='pl-10'>Coaching</Link>
+        <Link to="/#programmes" className='pl-10'>Programmes</Link>
         <NavLink to="/nutrition" className='pl-10'>Nutrition</NavLink>
         <NavLink to="/aboutme" className='pl-10'>About me</NavLink>
       </div>
