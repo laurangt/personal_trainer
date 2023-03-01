0ef61a3166b18c83545d67a481805e3012a66eea
Laura Nguyen Trong <laurangt@yahoo.com>
2023-03-01 10:24:11 +0100

    sandbox accounts payments working both sides

--- src/pages/PaypalCheckout.jsx
+++ src/pages/PaypalCheckout.jsx
@@ -1,127 +1,112 @@
-import React, {useEffect} from 'react'
-import { PayPalScriptProvider, PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js";
+import React, {useState} from 'react'
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+import { faHourglass2 } from '@fortawesome/free-regular-svg-icons';
 
 function PaypalCheckout() {
   const amount = "2";
   const description = "Program1"
   const currency = "USD";
-  const style = {"layout":"vertical"};
+  // const style = {"layout":"vertical"};
 
-  // const { product } = props
-  // const [paidFor, setPaidFor] = useState(false);
-  // const [error, setError] = useState(null);
+  const [show, setShow] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [orderID, setOrderID] = useState(false);
 
-  // const handleApprove = (orderId) => {
-  //   // Call backend function to fulfill order
-  //   // if response is success
-  //   if (Response.ok){
-  //     setPaidFor(true);
-  //     // Refresh user's account or subscription status
-  //   } else {
-  //     // if response is error
-  //     setError("Your payment was processed successfully. However, we are unable to fulfill your purchase. Please contact us at support@designcode.io for assistance.");
-  //   }
-  // };
-
-  // if (paidFor) {
-  //   // Display success message, modal or redirect user to success page
-  //   alert("Thank you for your purchase!");
-  // }
-
-  // if (error) {
-  //   // Display error message, modal or redirect user to error page
-  //   alert(error);
-  // }
-
-  // Custom component to wrap the PayPalButtons and handle currency changes
-  const ButtonWrapper = ({ currency, showSpinner }) => {
-    // usePayPalScriptReducer can be use only inside children of PayPalScriptProviders
-    // This is the main reason to wrap the PayPalButtons in a new component
-    const [{ options, isPending }, dispatch] = usePayPalScriptReducer();
-
-    useEffect(() => {
-        dispatch({
-            type: "resetOptions",
-            value: {
-                ...options,
-                currency: currency,
-            },
+  const createOrder = (data, actions) => {
+    return actions.order
+        .create({
+            purchase_units: [
+                {
+                  description: description,
+                  amount: {
+                      currency_code: "USD",
+                      value: 20,
+                  },
+                },
+            ],
+            application_context: {
+              shipping_preference: 'NO_SHIPPING'
+            }
+        })
+        .then((orderId) => {
+            setOrderID(orderID)
+            return orderId;
         });
-    }, [currency, showSpinner]);
+  };
+
+  const onApprove = (data, actions)  => {
+    return actions.order.capture().then(function (details) {
+      const {payer} = details;
+      setSuccess(true)
+      // alert("Transaction completed by " + details.payer.name.given_name)
+    });
+  };
+
+  const onError = (data, actions) => {
+    setErrorMessage("An error occured with your payment");
+  }
+
+  // const ButtonWrapper = ({ currency, showSpinner }) => {
+  //   const [{ options, isPending }, dispatch] = usePayPalScriptReducer();
+
+  //   useEffect(() => {
+  //     dispatch({
+  //         type: "resetOptions",
+  //         value: {
+  //           ...options,
+  //           currency: currency,
+  //         },
+  //     });
+  //   }, [currency, showSpinner]);
 
 
-    return (<>
-      { (showSpinner && isPending) && <div className="spinner" /> }
-      <PayPalButtons
-          style={style}
-          disabled={false}
-          forceReRender={[amount, currency, style]}
-          fundingSource={undefined}
-          createOrder={(data, actions) => {
-            return actions.order
-                .create({
-                    purchase_units: [
-                        {
-                          description: description,
-                          amount: {
-                              currency_code: currency,
-                              value: amount,
-                          },
-                        },
-                    ],
-                })
-                .then((orderId) => {
-                    // Your code here after create the order
-                    return orderId;
-                });
-          }}
-
-          onApprove={function (data, actions) {
-            return actions.order.capture().then(function (details) {
-                // Your code here after capture the order
-              alert("Transaction completed by " + details.payer.name.given_name)
-              // handleApprove(data.orderID)
-            });
-          }}
-
-          // onError={(err) => {
-          //   setError(err);
-          //   console.error("PayPal Checkout onError", err);
-          // }}
-
-          // onCancel={() => {
-          //   // Display cancel message, modal or redirect user to cancel page or back to cart
-          //   alert("You have canceled your order")
-          // }}
-        />
-      </>
-    );
-}
-
+  //   return (<>
+  //     { (showSpinner && isPending) && <div className="spinner" /> }
+  //     <PayPalButtons
+  //         disabled={false}
+  //         forceReRender={[amount, currency, style]}
+  //         fundingSource={undefined}
+  //         style={{layout: "vertical"}}
+  //         createOrder={createOrder}
+  //         onApprove={onApprove}
+  //         onError={onError}
+  //       />
+  //     </>
+  //   );
+  // }
 
   return (
-    <div className='flex flex-col justify-center text-center mt-20'>
-      <h1 className='text-2xl'>Program</h1>
-      <p>$10.00</p>
-      <PayPalScriptProvider options={{"client-id": process.env.REACT_APP_CLIENT_ID}}>
-        <div style={{ maxWidth: "750px", minHeight: "200px" }}>
-          <PayPalScriptProvider
-            options={{
-              "client-id": "test",
-              components: "buttons",
-              currency: "USD"
-            }}
-          >
-            <ButtonWrapper
-              currency={currency}
-              showSpinner={false}
-            />
-          </PayPalScriptProvider>
-        </div>
+    <div>
+      <PayPalScriptProvider
+        options={{
+          "client-id": process.env.REACT_APP_CLIENT_ID
+        }}
+      >
+        <h1>Program1</h1>
+        <span>20€</span>
+        <button onClick={() => setShow(true) } type="submit">Buy now</button>
+
+        {show ? (
+          <PayPalButtons
+            style={{layout: "vertical"}}
+            createOrder={createOrder}
+            onApprove={onApprove}
+            onError={onError}
+          />
+        ) : null}
+
+        {success ? (
+          <h1>Your Payment has been done successfully please check email</h1>
+        ) : null}
+
+        {/* <ButtonWrapper
+          currency={currency}
+          showSpinner={false}
+        />
+        <button type="submit" onClick={() => setShow(true)} className='bg-black text-white p-5'>Buy now</button> */}
+
       </PayPalScriptProvider>
-      <form>
-        <button type="submit" className='bg-black text-white p-5'>Buy now</button>
-      </form>
     </div>
   )
 }
